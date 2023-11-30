14677d6b0019d51cb69936149b3faf846b68e4c3
Vitor <vitorturqueti@hotmail.com>
2023-11-30 16:41:23 -0300

    changed if_block dropdown field to number field

--- front/blocks.js
+++ front/blocks.js
@@ -1,3 +1,5 @@
+const avisoPilhas = document.getElementById("warningDiv")
+
 Blockly.Blocks["movimentos"] = {
     init: function () {
         this.appendDummyInput()
@@ -15,9 +17,49 @@ Blockly.Blocks["movimentos"] = {
         this.setPreviousStatement(true, null)
         this.setNextStatement(true, null)
         this.setColour(225)
-        this.setTooltip("")
-        this.setHelpUrl("")
+        this.setTooltip("Bloco de movimento")
     },
+}
+
+Blockly.Blocks["if_block"] = {
+    init: function() {
+        this.appendDummyInput("se")
+            .appendField("Se sensor de distância menor ou igual a ")
+            .appendField(new Blockly.FieldNumber(1, 1, 9, 1), "distancia")
+            .appendField("cm")
+        const input = this.appendStatementInput("if")
+        input.appendField("faça")
+        input.setCheck(["movimentos", "loop_block"])
+
+        this.setInputsInline(true)
+        this.setPreviousStatement(true, "if_block")
+        this.setNextStatement(true, null)
+        this.setColour(160)
+        this.setTooltip("Bloco de condição")
+
+        this.setOnChange(function(event){
+            let reason = event.reason
+            if(reason){
+                if(reason.includes('connect')){
+                    let blocoDentroInput = this.getInputTargetBlock('if');
+                    let temBlocoIf = false
+                    if(blocoDentroInput){
+                        do{ 
+                            if(blocoDentroInput.type === 'if_block'){
+                                temBlocoIf = true
+                                break
+                            }
+                        }while (blocoDentroInput = blocoDentroInput.getNextBlock());
+                        if(temBlocoIf){
+                            exibirAviso("Ifs aninhados ou vazios não serão considerados!")
+                        }
+                    }
+                }
+            }
+            
+        }) 
+        
+    }
 }
   
 Blockly.Blocks["loop_block"] = {
@@ -27,13 +69,42 @@ Blockly.Blocks["loop_block"] = {
             .appendField(new Blockly.FieldNumber(1, 1), "TIMES")
         const input = this.appendStatementInput("DO")
         input.appendField("vez(es)")
-        input.setCheck("movimentos")
+        input.setCheck(["movimentos", "if_block"])
+
         this.setPreviousStatement(true, "loop_block")
-        this.setNextStatement(true, "movimentos")
+        this.setNextStatement(true, null)
         this.setColour(90)
-        this.setTooltip("")
-        this.setHelpUrl("")
+        this.setTooltip("Bloco de repetição simples (ATENÇÃO: blocos de loop aninhados serão ignorados).")
+
+        this.setOnChange(function(event){
+            let reason = event.reason
+            if(reason){
+                if(reason.includes('connect')){
+                    let blocoDentroInput = this.getInputTargetBlock('DO');
+                    let temBlocosRepeticao = false
+                    if(blocoDentroInput){
+                        do{ 
+                            if(blocoDentroInput.type === 'loop_block'){
+                                temBlocosRepeticao = true
+                                break
+                            }
+                        }while (blocoDentroInput = blocoDentroInput.getNextBlock());
+                        if(temBlocosRepeticao){
+                            exibirAviso("Loops aninhados ou vazios não serão considerados!")
+                        }
+                    }
+                }
+            }
+            
+        }) 
+
+        
     }
+}
+
+function exibirAviso(aviso){
+    erroPilhas.innerHTML = '&nbsp'
+    avisoPilhas.innerHTML = aviso
 }
 
 const toolbox = {
@@ -46,6 +117,10 @@ const toolbox = {
         {
             "kind": "block",
             "type": "loop_block"
+        },
+        {
+            "kind": "block",
+            "type": "if_block"
         }
     ]
 }
